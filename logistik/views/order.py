220c627e481b1adc84ac5a0f236376fde9d85246
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-03-16 22:11:26 +0800

    added swag_from in order fulfillment view

--- logistik/views/order.py
+++ logistik/views/order.py
@@ -1,34 +1,15 @@
+import os
 from flask import jsonify, Blueprint
 from .specs import BASE_PREFIX
 from ..exceptions import InsufficientStockError, OrderNotFoundError
 from ..models import Order
+from flasgger import swag_from
 
 bp_order = Blueprint('custom', __name__, url_prefix=BASE_PREFIX)
 
-
+@swag_from("{}/swagger/order.yml".format(os.getcwd()), validation=True)
 @bp_order.route('order/<int:order_id>/fulfill')
 def fulfill_order(order_id):
-    """
-    View function for order fulfillment.
-
-    Params:
-    -------
-      - order_id(int): Id of order that needs to be fulfilled.
-
-    Example Invokcation:
-    --------------------
-      - /api/v1.0/order/1/fulfill
-
-    Response:
-    ---------
-      - [
-          {
-            "id": 1,
-            "quantity": 5
-          },
-          ...
-        ]
-    """
     status_code = 200
     response = {}
 
