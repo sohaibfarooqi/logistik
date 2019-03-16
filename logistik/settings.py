e9072c1ef2d437a2caf1eacd7a995dc54cdcb840
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-03-16 14:55:42 +0800

    added SERVER_NAME for test env

--- logistik/settings.py
+++ logistik/settings.py
@@ -8,7 +8,7 @@ class Settings(object):
   """
   DEBUG = False
   TESTING = False
-  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'local.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionSettings(Settings):
@@ -27,4 +27,6 @@ class TestingSettings(Settings):
   """
   Use this class to override any special settings for testing
   """
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'testing.db')
   TESTING = True
+  SERVER_NAME = "127.0.0.1:5555"
