0381ce05c12e6f909137821c187699a8edf8bdd0
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-03-16 16:33:48 +0800

    added

--- logistik/models/sku.py
+++ logistik/models/sku.py
@@ -7,6 +7,8 @@ class Sku(Base):
     """
     Class definition of Sku. Definies product name
     """
+    _exclude = ('skus', 'storages')
+
     product_name = db.Column(db.String, nullable=False)
     storages = db.relationship(
         'Storage', secondary=available_skus, order_by='Storage.stock', lazy='subquery')
--- logistik/models/storage.py
+++ logistik/models/storage.py
@@ -7,5 +7,7 @@ class Storage(Base):
     """
     Class definition of Storage(warehouse). Defines sku and avaiable stock.
     """
+    _exclude = ('skus',)
+
     stock = db.Column(db.Integer, nullable=False)
     skus = db.relationship('Sku', secondary=available_skus, lazy='subquery')
